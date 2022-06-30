7147b76c7996c70dff660556f979ac3378ad99e7
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-06-30 15:36:38 +0530

    feat: add project images

--- src/components/ProjectsDone/ProjectsDone.jsx
+++ src/components/ProjectsDone/ProjectsDone.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -6,21 +6,17 @@ import Button from "@mui/material/Button";
 import metamarketing from "../../assets/metamarketing.png";
 import paperPlane from "../../assets/paper-plane.png";
 import globe from "../../assets/globe.png";
+import { navData } from "./data";
 import styles from "./ProjectsDone.module.css";
 
 const ProjectsDone = () => {
-  const [activeNav, setActiveNav] = useState("DAPPs");
-  const navData = ["DAPPs", "Contracts", "NFT's", "Websites"];
-  const cardData = [
-    {
-      header: "COSMIC",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing adipiscing elit adipiscing adipiscing elit ",
-    },
-    {
-      header: "Rocket Global",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing adipiscing elit adipiscing adipiscing elit ",
-    },
-  ];
+  const [activeNav, setActiveNav] = useState(Object.keys(navData)[0]);
+  const [activeCardData, setActiveCardData] = useState();
+
+  useEffect(() => {
+    setActiveCardData(navData[activeNav]);
+  }, [activeNav]);
+
   return (
     <Container
       maxWidth="xl"
@@ -86,7 +82,7 @@ const ProjectsDone = () => {
             scrollbarWidth: "thin",
           }}
         >
-          {navData.map((item) => {
+          {Object.keys(navData).map((item) => {
             return (
               <Button
                 className={styles.primaryText}
@@ -124,125 +120,103 @@ const ProjectsDone = () => {
           flexWrap: "wrap",
           justifyContent: { xs: "center", md: "evenly" },
           mt: { xs: "4rem", md: 2 },
-          width: "100%",
         }}
       >
-        {cardData.map((card) => {
-          return (
-            <Box
-              className={styles.card}
-              key={card.header}
-              sx={{
-                height: "auto",
-                width: "100%",
-                maxWidth: { xs: "90%", sm: "80%", md: "max( 40% , 475px)" },
-                mx: { xs: "auto", md: 3 },
-                display: "flex",
-                flexDirection: "column",
-                flexWrap: { xs: "wrap", sm: "nowrap" },
-                justifyContent: "space-around",
-                borderRadius: "20px",
-                p: { xs: "auto", sm: 2.5 },
-                py: { xs: 2 },
-                my: 3,
-              }}
-            >
+        {activeCardData &&
+          activeCardData.map((card) => {
+            return (
               <Box
+                className={styles.card}
+                key={card.header}
                 sx={{
+                  height: "auto",
+                  width: "100%",
+                  maxWidth: { xs: "90%", sm: "80%", md: "max( 40% , 475px)" },
+                  mx: { xs: "auto", md: 3 },
                   display: "flex",
                   flexDirection: "column",
-                  justifyContent: "center",
-                  p: { xs: 1, sm: 0 },
+                  flexWrap: { xs: "wrap", sm: "nowrap" },
+                  justifyContent: "space-around",
+                  borderRadius: "20px",
+                  p: { xs: "auto", sm: 2 },
+                  py: { xs: "auto" },
+                  my: 3,
                 }}
               >
                 <Box
+                  component="img"
+                  alt={card.header}
+                  src={card.image}
+                  sx={{
+                    borderRadius: "20px",
+                    width: "100%",
+                    height: "auto",
+                  }}
+                />
+                <Box
                   sx={{
                     display: "flex",
-                    flexDirection: "row",
-                    alignItems: "center",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    p: { xs: 1, sm: 0 },
+                    mt: { xs: 0, sm: 1 },
                   }}
                 >
                   <Box
-                    component="img"
-                    src={metamarketing}
-                    alt="icon"
                     sx={{
-                      height: { xs: "25px", sm: "30px" },
-                      width: { xs: "42px", sm: "55px" },
-                    }}
-                  />
-                  <Typography
-                    variant="h6"
-                    className={styles.primaryText}
-                    sx={{
-                      fontFamily: "Montserrat",
-                      fontWeight: 600,
-                      fontSize: { xs: "1.1rem", sm: "1.3rem" },
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
                     }}
                   >
-                    {card.header}
-                  </Typography>
-                  <Box flexGrow={1} />
-                  <Box
-                    className={styles.image}
-                    component="img"
-                    src={paperPlane}
-                    alt="paper plane"
-                    sx={{
-                      height: { xs: "23px", sm: "27px" },
-                      width: { xs: "23px", sm: "27px" },
-                      mx: 0.5,
-                    }}
-                  />
-                  <Box
-                    className={styles.image}
-                    component="img"
-                    src={globe}
-                    alt="globe"
-                    sx={{
-                      height: { xs: "23px", sm: "27px" },
-                      width: { xs: "23px", sm: "27px" },
-                      mx: 0.5,
-                    }}
-                  />
+                    <Box
+                      component="img"
+                      src={metamarketing}
+                      alt="icon"
+                      sx={{
+                        height: { xs: "25px", sm: "30px" },
+                        width: { xs: "42px", sm: "55px" },
+                      }}
+                    />
+                    <Typography
+                      variant="h6"
+                      className={styles.primaryText}
+                      sx={{
+                        fontFamily: "Montserrat",
+                        fontWeight: 600,
+                        fontSize: { xs: "1.1rem", sm: "1.3rem" },
+                      }}
+                    >
+                      {card.header}
+                    </Typography>
+                    <Box flexGrow={1} />
+                    <Box
+                      className={styles.image}
+                      component="img"
+                      src={paperPlane}
+                      alt="paper plane"
+                      sx={{
+                        height: { xs: "23px", sm: "27px" },
+                        width: { xs: "23px", sm: "27px" },
+                        mx: 0.5,
+                      }}
+                    />
+                    <Box
+                      className={styles.image}
+                      component="img"
+                      src={globe}
+                      alt="globe"
+                      sx={{
+                        height: { xs: "23px", sm: "27px" },
+                        width: { xs: "23px", sm: "27px" },
+                        mx: 0.5,
+                      }}
+                    />
+                  </Box>
                 </Box>
-                <Typography
-                  variant="h6"
-                  className={styles.secondaryText}
-                  sx={{
-                    fontFamily: "Poppins",
-                    fontWeight: 500,
-                    fontSize: { xs: "0.8rem", sm: "0.95rem" },
-                    ml: "8px",
-                    mt: 1,
-                  }}
-                >
-                  {card.text}
-                </Typography>
               </Box>
-              <Button
-                sx={{
-                  ml: { xs: 2, sm: 1 },
-                  mt: 2,
-                  backgroundColor: "#00D186",
-                  color: "black",
-                  display: "block",
-                  fontSize: "0.9rem",
-                  fontWeight: 500,
-                  fontFamily: "Poppins",
-                  maxWidth: "150px",
-                  p: "1px 4px",
-                  borderRadius: "7px",
-                  "&:hover": {
-                    backgroundColor: "#00D186",
-                  },
-                }}
-              >
-                KNOW MORE
-              </Button>
-            </Box>
-          );
-        })}
+            );
+          })}
       </Box>
     </Container>
   );
---
+++ src/components/ProjectsDone/data.js
@@ -0,0 +1,73 @@
+import amberProtocol from "../../assets/screenshots/amberProtocol.png";
+import cosmik from "../../assets/screenshots/cosmik.png";
+import lMeta from "../../assets/screenshots/lMeta.png";
+import metaMarketing from "../../assets/screenshots/metaMarketing.png";
+import safeEarn from "../../assets/screenshots/safeEarn.png";
+import cosmikCalculator from "../../assets/screenshots/cosmikCalculator.png";
+import safeEarnCalculator from "../../assets/screenshots/safeEarnCalculator.png";
+import qingLongInu from "../../assets/screenshots/qingLongInu.png";
+import metaMarketingStaking from "../../assets/screenshots/metaMarketingStaking.png";
+import rocketGlobal from "../../assets/screenshots/rocketGlobal.png";
+import luckyMetaverse from "../../assets/screenshots/luckyMetaverse.png";
+import metaLoop from "../../assets/screenshots/metaLoop.png";
+
+export const navData = {
+  "NFT Minting Websites": [
+    {
+      header: "Quing Long Inu",
+      image: qingLongInu,
+    },
+  ],
+  Dashboards: [
+    {
+      header: "Amber Protocol Dashboard",
+      image: amberProtocol,
+    },
+    {
+      header: "Cosmik Dashboard",
+      image: cosmik,
+    },
+    {
+      header: "LMeta Dashboard",
+      image: lMeta,
+    },
+    {
+      header: "Meta Marketing Dashboard",
+      image: metaMarketing,
+    },
+    {
+      header: "Safe Earn Dashboard",
+      image: safeEarn,
+    },
+  ],
+  "Staking Websites": [
+    {
+      header: "Meta Marketing Staking",
+      image: metaMarketingStaking,
+    },
+  ],
+  Calculators: [
+    {
+      header: "Cosmik Calculator",
+      image: cosmikCalculator,
+    },
+    {
+      header: "Safe Earn Calculator",
+      image: safeEarnCalculator,
+    },
+  ],
+  Websites: [
+    {
+      header: "Rocket Global",
+      image: rocketGlobal,
+    },
+    {
+      header: "Lucky Metaverse",
+      image: luckyMetaverse,
+    },
+    {
+      header: "Meta Loop",
+      image: metaLoop,
+    },
+  ],
+};
